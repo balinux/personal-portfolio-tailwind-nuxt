c7707f984d9b8468c3a5ba04a7cb4e3bcfbf3612
balinux <ryojuniyantara@gmail.com>
2022-11-05 11:00:57 +0800

    udpate: navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
- <nav class=" flex flex-row h-20 w-screen justify-between items-center px-20">
+ <nav class=" flex flex-row h-20 w-screen justify-between items-center px-5 md:px-20">
     <p class=" font-bold text-4xl text-[#FECE47]"> R.</p>
-    <div class="flex flex-row">
+    <div class="md:flex flex-row hidden">
         <p class="mr-5">Home</p>
         <p class="mr-5">About Me</p>
         <p class="mr-5">Hobby</p>
